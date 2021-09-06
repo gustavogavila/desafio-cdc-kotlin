d327009d47dc6ceda1c3f94ac3aace4506708597
Gustavo Avila <gustavogavila01@gmail.com>
2021-09-06 15:11:35 -0300

    commit inicial

---
+++ src/test/kotlin/br/com/gusta/cdc/CdcApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.gusta.cdc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CdcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
